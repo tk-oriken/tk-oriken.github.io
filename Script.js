abf5075bed18404321bb46b85793566277f7a1ad
tk-oriken <TK.OrigamiClub@gmail.com>
2025-07-27 22:55:30 +0900

    y

---
+++ Script.js
@@ -0,0 +1,19 @@
+//jsonファイルのあるディレクトリのパスとファイル情報(index.json)のパス
+//いじるな
+const basePath = 'https://tk-oriken.github.io/data/';
+const indexURL = `${basePath}index.json`;
+
+fetch(indexUrl)
+  .then(res => res.json())
+  .then(fileList => {
+    fileList.forEach(fileName => {
+        const img = document.createElement('img');
+        img.src = basePath + fileName + ".png";
+        img.alt = fileName;
+        img.width = 200;
+        document.getElementById('list').appendChild(img);
+    });
+  })
+  .catch(err => {
+    console.error('index.json の取得に失敗しました', err);
+  });
